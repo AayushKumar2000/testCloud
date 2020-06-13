ea00833748a1623445805dcc81f7a62fafc0fc62
AayushKumar2000 <46739313+AayushKumar2000@users.noreply.github.com>
2020-06-13 12:57:05 +0530

    created indes.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const app=require('express')();
+const {exec} =require('child_process');
+
+app.get('/',(req,res)=>{
+        res.send('hi hello ----------------');
+});
+
+app.post('/gitAlert',(req,res)=>{
+        console.log('git alert');
+      exec('sudo rm -rf /var/www/html/*');
+     exec('sudo rm -rf /var/www/html/.git');
+      exec('sudo git clone https://github.com/AayushKumar2000/testCloud.git /var/www/html/');
+          console.log('rready');
+        res.send();
+});
